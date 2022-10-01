f4c7ecd3a9ff2897456bb5d7f3679ae218b4d1a1
taro <lautaro@airtm.io>
2022-10-01 19:52:19 -0300

    primitive connection + user list

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Button from "@mui/material/Button";
+import { UserList } from './UserList'
+
+// const serverUrl = 'wss://nooks-takehome-backend-production.up.railway.app'
+const serverUrl = 'ws://localhost:8080'
 
 // randomly generate a user ID every time you join the room
 // you don't need persistence between browser reloads or different sessions,
 // so a random ID will do to distinguish between two tabs with the Youtube Watch Party Open
-const USER_ID = uuidv4();
+const userId = uuidv4();
 
 function App() {
+  const [users, setUsers] = useState<string[]>([])
+
+  useEffect(() => {
+    const socket = new WebSocket(serverUrl)
+
+    socket.addEventListener('open', () => {
+      console.log('socket opened')
+      socket.send(JSON.stringify({
+        action: 'join',
+        userId,
+      }))
+    })
+
+    socket.addEventListener('message', (message) => {
+      const messageData = JSON.parse(message.data)
+
+      if (messageData.action === 'join') {
+        setUsers([...users, messageData.userId])
+      } else {
+        console.log('unknown message', messageData)
+      }
+    })
+
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Welcome to the Youtube Watch Party. Your ID for this session is{" "}
-          {USER_ID}.
+          {userId}.
         </p>
         <Button> Add a youtube video</Button>
+        <UserList users={users} />
       </header>
     </div>
   );
