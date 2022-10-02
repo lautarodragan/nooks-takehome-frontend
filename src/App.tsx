fa2ff70c64c7498db5920fd0c7d35bf8556ae319
taro <lautaro@airtm.io>
2022-10-01 21:12:47 -0300

    primitive YouTubeURLInput component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import Button from "@mui/material/Button";
 import { UserList } from './UserList'
 import { useWebSocket } from './useWebSocket'
+import { YouTubeURLInput } from './YouTubeURLInput'
 
 const serverUrl = process.env.REACT_APP_SERVER_URL || 'ws://localhost:8080'
 
@@ -11,17 +12,15 @@ const serverUrl = process.env.REACT_APP_SERVER_URL || 'ws://localhost:8080'
 // so a random ID will do to distinguish between two tabs with the Youtube Watch Party Open
 const userId = uuidv4();
 
-const sampleVideos = [
-  'https://www.youtube.com/watch?v=kpk2tdsPh0A',
-  'https://www.youtube.com/watch?v=6EKreQ5HD4w',
-  'https://www.youtube.com/watch?v=qWXnt2Z2D1E',
-  'https://www.youtube.com/watch?v=0f0OvgfWrFQ',
-]
-
 function App() {
   const users = useWebSocket(serverUrl, userId)
 
-  const [videoUrl, setVideoUrl] = useState('')
+  const [videoId, setVideoId] = useState('')
+
+  const onUrlSubmit = (videoId: string) => {
+    console.log('submitted videoId', videoId)
+    setVideoId(videoId)
+  }
 
   return (
     <div className="App">
@@ -31,6 +30,7 @@ function App() {
           {userId}.
         </p>
         <Button> Add a youtube video</Button>
+        <YouTubeURLInput onSubmit={onUrlSubmit} />
         <UserList users={users} />
       </header>
     </div>
